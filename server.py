d75db32787f78c3d925c6eba989299d6b2d6be53
Liam Ha <haduylinh2005.work@gmail.com>
2024-12-15 20:06:20 -0500

    change port

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from app import create_app, db
+import os
 
 app = create_app()
-
+load_dotenv()
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         print("Tables created successfully!")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host=os.getenv("FLASK_RUN_HOST", "0.0.0.0"), port=int(os.getenv("FLASK_RUN_PORT", 5000)), debug=os.getenv("DEBUG", "False") == "True")
